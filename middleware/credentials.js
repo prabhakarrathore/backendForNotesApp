a4f5edd26e90176137055ae1d709863db55cbfff
prabhakar <prabhakarrathore15@gmail.com>
2024-10-21 13:43:00 +0530

    update credentials

--- middleware/credentials.js
+++ middleware/credentials.js
@@ -3,10 +3,13 @@ const allowedOrigin = require('../config/allowedOrigin');
 const credentials = (req, res, next) => {
     const origin = req.headers.origin;
     if (allowedOrigin.includes(origin)) {
-        res.header('Access-Control-Allow-Credientials', true);
+        res.header('Access-Control-Allow-Credentials', true);
     }
-    res.header('Cache-Control', 'no-store');
+    // Set Cache-Control header
+    res.header('Cache-Control', 'no-store'); // Adjust the value as needed
+    // Set X-Content-Type-Options header
+    res.header('X-Content-Type-Options', 'nosniff');
     next();
-}
+};
 
-module.exports = credentials
+module.exports = credentials;
