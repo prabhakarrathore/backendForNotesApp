1d307e992016125a7e3f08c0248d0eba30652934
prabhakar <prabhakarrathore15@gmail.com>
2024-10-21 13:48:17 +0530

    update ao with new link

--- config/allowedOrigin.js
+++ config/allowedOrigin.js
@@ -1,3 +1,3 @@
-const allowedOrigin = ['http://localhost:8000', 'http://localhost:3000','https://securenotesapp.netlify.app',"https://securenotes-mdwr39lfk-raman0001s-projects.vercel.app"];
+const allowedOrigin = ['http://localhost:8000', 'http://localhost:3000','https://securenotesapp.netlify.app',"https://securenotes-m3ex1y86q-raman0001s-projects.vercel.app"];
 
 module.exports = allowedOrigin;
