84ae4312e75608d3bbb482a55381c79e6eae5af9
prabhakar <prabhakarrathore15@gmail.com>
2024-10-21 11:48:38 +0530

    add vercel

--- config/allowedOrigin.js
+++ config/allowedOrigin.js
@@ -1,3 +1,3 @@
-const allowedOrigin = ['http://localhost:8000', 'http://localhost:3000','https://securenotesapp.netlify.app'];
+const allowedOrigin = ['http://localhost:8000', 'http://localhost:3000','https://securenotesapp.netlify.app',"https://securenotes-mdwr39lfk-raman0001s-projects.vercel.app"];
 
 module.exports = allowedOrigin;
